79b0175057e2e2cc313bc05d58125a12e5a57596
baramatsubonzo <baramatsubonzo@gmail.com>
2021-12-09 22:20:55 +0900

    abc019b

--- ABC085B/main.rb
+++ ABC085B/main.rb
@@ -1,8 +1,16 @@
-n = gets.chomp.to_i
-array = []
+array = gets.chomp.to_s.split('')
 
-n.times do
-  array << gets.chomp.to_i
+n = 1
+list = []
+array.each.with_index(1) do |ary, i|
+  if ary == array[i]
+    n += 1
+  else
+    list << ary
+    list << n.to_s
+    # 添字のnを元に戻す
+    n = 1
+  end
 end
 
-p array.uniq.size
+puts list.join
