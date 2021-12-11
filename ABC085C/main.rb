788e936adec9df72737cb2bc17177fa5b0ea25ba
baramatsubonzo <baramatsubonzo@gmail.com>
2021-12-11 14:24:51 +0900

    abc085 訂正

--- ABC085C/main.rb
+++ ABC085C/main.rb
@@ -1,14 +1,12 @@
 n, y = gets.chomp.split(' ').map(&:to_i)
 
-array = []
 (0..n).each do |a|
   (0..(n-a)).each do |b|
     c = n - a -b
     if 10000 * a + 5000 * b + 1000 * c == y && a + b + c == n
-      array << "#{a} #{b} #{c}"
-      break
+      puts "#{a} #{b} #{c}"
+      exit
     end
   end
 end
-
-puts array.empty? ? "-1 -1 -1": array.first
+puts "-1 -1 -1"
